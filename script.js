fc09f9da8fae986333d5252e38cf2785c70e3f26
Rafael Massiah <rafael.massiah13@gmail.com>
2023-04-21 21:23:35 -0300

    script to display results finished

---
+++ script.js
@@ -0,0 +1,25 @@
+let weather = {
+    "apiKey": "71fcf6cd03b8954c6f3402be88ef5e18",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid=" 
+        + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".temp").innerText =  temp + "°C";
+        document.querySelector(".description").innerText =  description;
+        document.querySelector(".humidity").innerText =  "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =  "Wind Speed: " + speed + "km/h";
+    }
+};
